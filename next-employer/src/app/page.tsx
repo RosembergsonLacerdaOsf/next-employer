b8c0ea6f8fdb119c9f737af39d2558eab7112acc
RosembergsonLacerdaOsf <rosembergson.lacerda@osf.digital>
2025-09-11 15:49:22 -0300

    [DOQR] List Employer
    Use component shadcn: Data Table;
    Implement component and styled;
    Try fix font-family's project.

--- next-employer/src/app/page.tsx
+++ next-employer/src/app/page.tsx
@@ -1,12 +1,14 @@
+import { DataTableDemo } from "./components/employer/employerDataTable";
 import Header from "./components/header/header";
 
 export default function Home() {
   return (
     <div className="font-display font-bold">
       <Header />
-      <div className="main-container lg:max-w-[1179px] m-auto sm:px-4">
-        <h1>Controle de Funcionários</h1>
-        <h2>Empresa DoQR Tecnologia</h2>
+      <div className="main-container lg:max-w-[1179px] m-auto px-4">
+        <h1 className="text-[36px] color-title">Controle de Funcionários</h1>
+        <h2 className="text-[20px] color-subtitle">Empresa DoQR Tecnologia</h2>
+        <DataTableDemo />
       </div>
     </div>
   );
